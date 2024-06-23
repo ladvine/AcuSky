169fad6d395f5a9f4e3a179dd464b113f6d25c5e
Ladvine D Almeida <ladvinealmeida.mec@gmail.com>
2024-06-23 19:58:31 +0530

    Update prebuild.sh

--- scripts/prebuild.sh
+++ scripts/prebuild.sh
@@ -6,9 +6,9 @@ mv arduino-1.8.19 $HOME/arduino_ide
 cd $HOME/arduino_ide/hardware
 mkdir esp32
 cd esp32
-wget https://github.com/espressif/arduino-esp32/archive/refs/tags/2.0.2.tar.gz
-tar -xzf 2.0.2.tar.gz
-mv arduino-esp32-2.0.2/ esp32
+wget https://github.com/espressif/arduino-esp32/archive/refs/tags/3.0.1.tar.gz
+tar -xzf 3.0.1.tar.gz
+mv arduino-esp32-3.0.1/ esp32
 cd esp32/tools
 python --version
 python get.py
